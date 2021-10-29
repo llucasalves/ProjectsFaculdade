d9f7fc674cd849403b8ab2aa2f091a133f75d4dd
Lucas <88686004+llucasalves@users.noreply.github.com>
2021-10-29 11:14:38 -0300

    Update exercicio01.py

--- Python 2021/exercicio01.py
+++ Python 2021/exercicio01.py
@@ -1,23 +1,23 @@
-""" Faça um Programa que converta metros para centímetros.
+Faça um Programa que converta metros para centímetros.
 
 metros = int(input('Digite a quantidade em metros: '))
-print(metros * 100) """
+print(metros * 100) 
 
 
-"""Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
 Calcule e mostre o total do seu salário no referido mês.
 
 hora = float(input('Quanto vc ganha por hora? '))
 mes = int(input('Horas trabalhada no mês? '))
-print('Voce Ganha R$', (hora*mes)) """
+print('Voce Ganha R$', (hora*mes)) 
 
 
-"""Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
 C = 5 * ((F-32) / 9).
 
 F = float(input('Digite uma temperatura em fahrenheit: '))
 C = 5 * ((F-32) / 9)
-print('A temperatura em Celsius seria: {}'.format(C))  """
+print('A temperatura em Celsius seria: {}'.format(C)) 
 
 
 
